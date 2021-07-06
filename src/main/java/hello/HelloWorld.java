6b16b63a72d535edcba0c7223f2cd8ad083264cb
MSFT_DEV_TEAM <msftgrabsharding@microsoft.com>
2021-07-06 13:08:34 +0000

    clear code

--- src/main/java/hello/HelloWorld.java
+++ src/main/java/hello/HelloWorld.java
@@ -1,27 +1,42 @@
 package hello;
 
+import java.util.Base64;
+import java.io.*;
 import org.joda.time.LocalTime;
 import com.azure.storage.*;
 import com.azure.storage.blob.*;
-import com.azure.identity.*;
+import com.azure.storage.blob.models.*;
+import com.azure.storage.common.StorageSharedKeyCredential;
+import com.azure.core.http.rest.*;
+import com.microsoft.azure.msiAuthTokenProvider.*;
 
 public class HelloWorld {
-    public static void main(String[] args) {
-      LocalTime currentTime = new LocalTime();
-		  System.out.println("The current local time is: " + currentTime);
-
-        Greeter greeter = new Greeter();
-        System.out.println(greeter.sayHello());
+    public static void main(String[] args) throws AzureMSICredentialException,IOException{
 
         String accountName  = "grabdemsftshardingaudit0";
         String endpoint=String.format("https://%s.blob.core.windows.net",accountName);
+     
+        MSICredentials credsProvider = MSICredentials.getMSICredentials();
+        credsProvider.updateClientId("3b62b63f-0895-44be-a050-c179c3c9b96d");
+        MSIToken  token = credsProvider.getToken(null);
+
+        StorageSharedKeyCredential credential = new StorageSharedKeyCredential(accountName, token.accessToken().toString());
+        BlobServiceClient blobStorageClient=new BlobServiceClientBuilder().endpoint(endpoint).credential(credential).buildClient();
+       
+        System.out.println("storage account name: "+ blobStorageClient.getAccountName());
+        System.out.println("storage account url: "+ blobStorageClient.getAccountUrl());
         
+
+    }
+}
+
+
+  /* 
         System.out.println(endpoint);
         BlobServiceClient blobStorageClient = new BlobServiceClientBuilder()
             .endpoint(endpoint)
             .credential(new DefaultAzureCredentialBuilder().build())
             .buildClient();
-        blobStorageClient.getAccountInfo();
-
-    }
-}
+        BlobContainerClient containerClient= blobStorageClient.getBlobContainerClient("insights-logs-storageread");
+        System.out.println("Container URL:"+containerClient.getBlobContainerUrl());
+        */
